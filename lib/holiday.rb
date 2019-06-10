4c2ffc02c6180983814e6302f90c9c194e5c1a8f
Andrew Davisson <akdavisson4@gmail.com>
2019-06-10 17:29:58 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,6 +66,11 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
 
+  holiday_hash.each do |season, holiday|
+    puts "#{season.to_s}:"
+    
+  end
+
 end
 
 def all_holidays_with_bbq(holiday_hash)
